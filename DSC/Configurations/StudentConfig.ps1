8b6ee9b48feb322a0d1febd6a4a385696e83e971
Mark Naylor <259908176+mgn1crt@users.noreply.github.com>
2026-05-05 20:54:45 +0100

    Corrected StudentConfig.psl

--- DSC/Configurations/StudentConfig.ps1
+++ DSC/Configurations/StudentConfig.ps1
@@ -61,7 +61,7 @@ Configuration StudentBaseline
 
     # Import modules.
     Import-DscResource -ModuleName PSDesiredStateConfiguration
-    Import-DscResource -ModuleName ComputerManagementDSC
+    # Import-DscResource -ModuleName ComputerManagementDSC
     #Import-DscResource -ModuleName ActivedirectoryDSC
 
     $nodes = $ConfigurationData.AllNodes
