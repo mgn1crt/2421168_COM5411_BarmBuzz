d2baac683e551a4382dee8c62eab39923292d47f
Mark Naylor <259908176+mgn1crt@users.noreply.github.com>
2026-05-06 01:44:44 +0100

    Updated AllNodes.psd1/StudentConfig.psl for networking configuration

--- DSC/Configurations/StudentConfig.ps1
+++ DSC/Configurations/StudentConfig.ps1
@@ -47,7 +47,7 @@ Configuration StudentBaseline
     param(
        # This section caused the key already used error and is therefore commented out.
        [Parameter(Mandatory)]
-       [hashtable]$ConfigurationData,
+       [hashtable]$ConfigurationData
 
        # [Parameter(Mandatory)]
        # [PSCredential]$DomainAdminCredential,
